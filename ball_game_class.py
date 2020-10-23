e71499ce9155b74d7cf91a7d6311199b4cc0a576
Greanneann <greanneann@gmail.com>
2020-10-23 11:17:41 +0300

    Доделала игру

--- ball_game_class.py
+++ ball_game_class.py
@@ -1,10 +1,10 @@
 from random import randint
 import pygame
-from pygame.draw import *
+from pygame.draw import circle, rect
 
 pygame.init()
 width, length = 1200, 900
-FPS = 8
+FPS = 10
 pygame.font.init()
 
 screen = pygame.display.set_mode((width, length))
@@ -28,15 +28,20 @@ class BaseTarget:
         self.screen = screen
         self.vx = randint(1, 10)
         self.vy = randint(1, 10)
+        self.angle = randint(1, 3)
 
     def move(self):
         self.x += self.vx
         self.y += self.vy
 
+    def special_move(self):
+        self.x += self.vx * self.angle
+        self.y += self.vy ** self.angle
+
     def check_bord(self):
-        if self.x >= width - 10 or self.x < 10:
+        if self.x > width - 10 or self.x < 10:
             self.vx *= -1
-        if self.y >= length - 10 or self.y < 10:
+        if self.y > length - 10 or self.y < 10:
             self.vy *= -1
 
 
@@ -53,7 +58,6 @@ class Circle(BaseTarget):
         a = (self.x - event_x) ** 2
         b = (self.y - event_y) ** 2
         c = a + b
-        print(c, self.R**2)
         if c <= self.R ** 2:
             return True
         return False
@@ -63,14 +67,14 @@ class Rectangle(BaseTarget):
     def __init__(self, screen):
         super().__init__(screen)
         self.width_2 = randint(30, 40)
-        self.height_2 = randint(30, 80)
+        self.lenght_2 = randint(30, 80)
 
     def draw(self):
-        rect(screen, self.color, (self.x, self.y, self.width_2, self.height_2))
+        rect(screen, self.color, (self.x, self.y, self.width_2, self.lenght_2))
 
     def check_event(self, pos):
         event_x, event_y = pos
-        if event_x - self.x <= self.width_2 and event_y - self.y <= self.height_2:
+        if (event_x - self.x < self.width_2) and (event_y - self.y) < self.lenght_2:
             return True
         return False
 
@@ -82,10 +86,11 @@ def give_score(score):
     file.close()
     print("Score: " + str(score))
 
+
 n_1 = 10
 balls = [Circle(screen) for _ in range(n_1)]
 n_2 = 3
-rects= [Rectangle(screen) for _ in range(n_2)]
+rects = [Rectangle(screen) for _ in range(n_2)]
 score = 0
 clock = pygame.time.Clock()
 finished = False
@@ -93,15 +98,15 @@ font = pygame.font.Font(None, 50)
 
 while not finished:
     clock.tick(FPS)
-   # for ball in balls:
-    #    ball.move()
-     #   ball.check_bord()
-      #s
-    #ball.draw()
-    for rect in rects:
-        rect.move()
-        rect.check_bord()
-        rect.draw()
+    for ball in balls:
+        ball.move()
+        ball.check_bord()
+        ball.draw()
+
+    for rectangle in rects:
+        rectangle.special_move()
+        rectangle.check_bord()
+        rectangle.draw()
 
     pygame.display.update()
     screen.fill(BLACK)
@@ -118,6 +123,13 @@ while not finished:
                     score += 1
                     balls.pop(i)
                     balls.append(Circle(screen))
+            for i, rectangle in enumerate(rects):
+                result = rectangle.check_event(event.pos)
+                if result:
+                    score += 3
+                    rects.pop(i)
+                    rects.append(Rectangle(screen))
 
 give_score(score)
 pygame.quit()
+
